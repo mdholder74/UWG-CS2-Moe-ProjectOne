cc92aad0e3ad68026822f5e395c60d4785123dcd
mdholder74 <mdholder74@gmail.com>
2025-09-24 07:09:28 -0400

    Business Logic for Storing a Task Con...
    6) Added input validation for name, description and priority in the Data constructor through exception throwing

--- code/JavaFX Sample Starter - AnchorPane/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/javafx_sample_starter/model/Data.java
+++ code/JavaFX Sample Starter - AnchorPane/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/javafx_sample_starter/model/Data.java
@@ -14,24 +14,68 @@ public class Data {
   
   private final String priority;
   
+/** Create a new task with the provided name, description, and priority.
+ * @precondition name != null && description != null && priority != null
+ * @postcondition getName() == name && getDescription() == description && getPriority() == priority
+ * @param name
+ * @param description
+ * @param priority
+ */
+  
   public Data(String name, String description, String priority) {
+    if (name == null || name.isEmpty()) {
+      throw new IllegalArgumentException("name must not be null or empty.");
+    }
+    
+    if (description == null || description.isEmpty()) {
+      throw new IllegalArgumentException("description must not be null or empty.");
+    }
+    
+    if (priority == null || priority.isEmpty()) {
+      throw new IllegalArgumentException("priority must not be null or empty.");
+    }
+    
     this.name = name;
     this.description = description;
     this.priority = priority;
   }
-  
+
+  /** Return the name of the task.
+   * @precondition none
+   * @postcondition none
+   * 
+   * @return the name of the task
+   */
   public String getName() {
     return this.name;
   }
   
+  /** Return the description of the task.
+   * @precondition none
+   * @postcondition none
+   * 
+   * @return the description of the task
+   */
   public String getDescription() {
     return this.description;
   }
   
+  /** Return the priority of the task.
+   * @precondition none
+   * @postcondition none
+   * 
+   * @return the priority of the task
+   */
   public String getPriority() {
     return this.priority;
   }
   
+  /** Return a string representation of the task name.
+   * @precondition none
+   * @postcondition none
+   * 
+   * @return a string representation of the task name
+   */
   @Override
   public String toString() {
     return this.name;
